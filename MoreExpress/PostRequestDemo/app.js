0fe459284bf75d5f227078fc4b23ad84969c9ebb
Sattar Hummatli <settarxan@gmail.com>
2017-10-30 09:51:07 +0000

    Played with POST routes

---
+++ MoreExpress/PostRequestDemo/app.js
@@ -0,0 +1,26 @@
+var express = require("express") 
+var app = express()
+var bodyParser = require("body-parser")
+
+var friends = ["Sattar", "Ali", "Hasan", "Fariz"]
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res){
+    res.render("home")
+})
+
+app.get("/friends", function(req, res){
+    res.render("friends", {friends: friends})
+})
+
+app.post("/addfriend", function(req, res){
+    var newFriend = req.body.newfriend
+    friends.push(newFriend)
+    res.redirect("/friends")
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Started server and listening...")
+})
