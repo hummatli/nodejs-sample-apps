c5c30ee8c18dadc38b228961359609468ba67914
Sattar Hummatli <settarxan@gmail.com>
2017-11-04 03:53:08 +0000

    Added register and authenticate functionality with PAssportJS

--- Authentification/AuthDemo/app.js
+++ Authentification/AuthDemo/app.js
@@ -6,12 +6,19 @@ var express                 = require("express"),
     LocalStartegy           = require("passport-local"),
     passportLocalMongoose   = require("passport-local-mongoose")
 
-mongoose.connect("mongodb://localhost/auth_demo_app" , { useMongoClient: true })
+mongoose.connect("mongodb://localhost/auth_demo_app" , { useMongoClient: true }, function(err) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log("CONNECTED TO DB")
+    }
+})
 mongoose.Promise = global.Promise
 
 var app = express()
 app.set("view engine", "ejs")
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(require("express-session")({
     secret: "Rusty is the best and cutest dog in the world",
     resave: false,
@@ -23,13 +30,37 @@ app.use(passport.session())
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-
+//=============
+//ROUTES
+//=============
 app.get("/", function(req, res) {
     res.render("home")
 })
 
 app.get("/secret", function(req, res) {
     res.render("secret")
+})
+
+//Auth Routes
+
+//show sign up form
+app.get("/register", function(req, res) {
+    res.render("register")
+})
+//handling user sign up
+app.post("/register", function(req, res) {
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+    console.log("registered")
+        if(err) {
+            console.log(err)
+            return res.render("register")
+        }
+        passport.authenticate("local")(req, res, function() {
+            console.log("authenticated")
+            res.redirect("/secret")
+        }) 
+    })
+    
 })
 
 app.listen(process.env.PORT, process.env.IP, function() {
